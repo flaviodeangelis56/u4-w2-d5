aff0bf429f8a0a318169f2c8df3ff707f3587702
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-10-13 11:14:50 +0200

    add-AddFunction
    aggiunto la possibilità di aggiungere elementi all' archivio

--- src/main/java/flaviodeangelis/Application.java
+++ src/main/java/flaviodeangelis/Application.java
@@ -1,34 +1,67 @@
 package flaviodeangelis;
 
+import flaviodeangelis.entities.Books;
 import flaviodeangelis.entities.Magazine;
 import flaviodeangelis.entities.Periodicità;
 import flaviodeangelis.entities.element;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         List<element> archivio = new ArrayList<>();
-        Magazine test1 = new Magazine("test1", 2023, 125, Periodicità.SETTIMANALE);
-        archivio.add(test1);
+
+// --------------------------------------- ADD --------------------------------------
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci book per aggiunere un libro, magazine per aggiungere una rivista");
+        String inputCreazione = input.nextLine();
+        if (inputCreazione.toLowerCase().trim().equals("book")) {
+            System.out.println("Come si chiama il tuo libro : ");
+            String inputTitle = input.nextLine();
+            System.out.println("Qual'è l'anno di publicazione : ");
+            int inputYear = Integer.parseInt(input.nextLine());
+            System.out.println("Quante pagine ha : ");
+            int inputPage = Integer.parseInt(input.nextLine());
+            System.out.println("Chi è l'autore : ");
+            String inputAuthor = input.nextLine();
+            System.out.println("Di che genere è : ");
+            String inputType = input.nextLine();
+            Books book = new Books(inputTitle, inputYear, inputPage, inputAuthor, inputType);
+            archivio.add(book);
+        } else if (inputCreazione.toLowerCase().trim().equals("magazine")) {
+            System.out.println("Come si chiama la tua rivista : ");
+            String inputTitle = input.nextLine();
+            System.out.println("Qual'è l'anno di publicazione : ");
+            int inputYear = Integer.parseInt(input.nextLine());
+            System.out.println("Quante pagine ha : ");
+            int inputPage = Integer.parseInt(input.nextLine());
+            System.out.println("Qual'è la sua periodicità (periodicità disponibili settimanale,mensile,semestrale) : ");
+            String inputPeriod = input.nextLine();
+            if (inputPeriod.toLowerCase().trim().equals("settimanale")) {
+                Magazine magazine = new Magazine(inputTitle, inputYear, inputPage, Periodicità.SETTIMANALE);
+                archivio.add(magazine);
+            } else if (inputPeriod.toLowerCase().trim().equals("mensile")) {
+                Magazine magazine = new Magazine(inputTitle, inputYear, inputPage, Periodicità.MENSILE);
+                archivio.add(magazine);
+            } else if (inputPeriod.toLowerCase().trim().equals("semestrale")) {
+                Magazine magazine = new Magazine(inputTitle, inputYear, inputPage, Periodicità.SEMESTRALE);
+                archivio.add(magazine);
+            }
+        } else {
+            System.out.println("Tipo di elemento non disponibile");
+        }
+// ------------------------------------------- REMOVE ----------------------------------------------
+
         System.out.println(archivio);
     }
 
-    public static void addAnElement(String bOrM) {
 
-    }
-
-    public static void addAMagazine(String title, int yearOfPublication, int numberOfPage, String periodicità) {
-        if (periodicità.equals("settimanale")) {
-            Magazine magazine = new Magazine(title, yearOfPublication, numberOfPage, Periodicità.SETTIMANALE);
-            archivio.add(magazine);
-        } else if (periodicità.equals("mensile")) {
-            Magazine magazine = new Magazine(title, yearOfPublication, numberOfPage, Periodicità.MENSILE);
-        } else if (periodicità.equals("semestrale")) {
-            Magazine magazine = new Magazine(title, yearOfPublication, numberOfPage, Periodicità.SEMESTRALE);
-        }
-    }
 }
+
+
+
+
